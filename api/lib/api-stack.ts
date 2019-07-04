efa57b8887a9cc8ddbbfec604a1553299df77e61
Matthew Bonig <matthew.bonig@gmail.com>
2019-07-04 10:06:38 -0600

    first run of cicd and api

---
+++ api/lib/api-stack.ts
@@ -0,0 +1,23 @@
+import cdk = require('@aws-cdk/core');
+import { Table, AttributeType } from '@aws-cdk/aws-dynamodb';
+
+export class ApiStack extends cdk.Stack {
+  eventsTable: Table;
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const buildAPIGateway = this.node.tryGetContext('api-gateway-enabled') || false;
+
+    this.buildDatabase();
+    this.buildAggregators();
+  }
+  buildDatabase() {
+    this.eventsTable = new Table(this, 'events-table', {
+      partitionKey: { name: 'eventId', type: AttributeType.STRING },
+      sortKey: { name: 'timestamp', type: AttributeType.STRING }
+
+    })
+  }
+  buildAggregators() {
+  }
+}
---
+++ cicd/lib/cicd-stack.ts
@@ -0,0 +1,81 @@
+import cdk = require('@aws-cdk/core');
+import { Pipeline, Artifact } from '@aws-cdk/aws-codepipeline';
+import { GitHubSourceAction, CodeBuildAction } from '@aws-cdk/aws-codepipeline-actions';
+import { Project, BuildSpec, PipelineProject, LinuxBuildImage, ComputeType } from '@aws-cdk/aws-codebuild'
+
+
+export class CicdStack extends cdk.Stack {
+  pipeline: Pipeline;
+  projectName: string;
+
+  static PASSTHROUGH_BUILDSPEC: any = {
+    version: '0.2',
+    phases: {
+      build: {
+        commands: [
+          'env',
+        ],
+      },
+    },
+    artifacts: {
+      'files': [
+        '**/*',
+      ],
+    },
+  };
+
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    this.projectName = 'simple-es-cicd'
+    this.setupGithubSource();
+    this.setupCodeBuildProject();
+    this.setupCodePipeline();
+  }
+  setupGithubSource() {
+  }
+  setupCodeBuildProject() {
+  }
+  setupCodePipeline() {
+    const branch = this.node.tryGetContext('branch') || 'master';
+    const owner = this.node.tryGetContext('owner');
+    const repo = this.node.tryGetContext('repo');
+    const oauthToken = this.node.tryGetContext('oauthToken');
+
+    const githubSource = new Artifact('github-source');
+    const deployArtifacts = new Artifact('deploy-artifacts')
+
+    this.pipeline = new Pipeline(this, this.projectName, {
+      stages: [
+        {
+          stageName: 'source',
+          actions: [new GitHubSourceAction({
+            branch,
+            owner,
+            repo,
+            oauthToken,
+            output: githubSource,
+            actionName: 'clone'
+          })]
+        },
+        {
+          stageName: 'Build',
+          actions: [
+            new CodeBuildAction({
+              actionName: 'build',
+              input: githubSource,
+              outputs: [deployArtifacts],
+              project: new PipelineProject(this, `${this.projectName}-codebuild`, {
+                buildSpec: BuildSpec.fromSourceFilename('api/buildspec.yaml'),
+                environment: {
+                  buildImage: LinuxBuildImage.UBUNTU_14_04_DOCKER_18_09_0,
+                  computeType: ComputeType.SMALL
+                },
+              })
+            })
+          ]
+        }
+      ]
+    });
+  }
+}
