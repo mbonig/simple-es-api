d72b02c8f4db025f824154639eebffd2953db458
Matthew Bonig <matthew.bonig@gmail.com>
2019-07-04 13:55:16 -0600

    updating buildspec.yaml

--- api/lib/api-stack.ts
+++ api/lib/api-stack.ts
@@ -3,21 +3,33 @@ import { Table, AttributeType } from '@aws-cdk/aws-dynamodb';
 
 export class ApiStack extends cdk.Stack {
   eventsTable: Table;
+
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const buildAPIGateway = this.node.tryGetContext('api-gateway-enabled') || false;
 
+
+
     this.buildDatabase();
     this.buildAggregators();
+    if (buildAPIGateway) {
+      this.buildAPIGateway();
+    }
   }
+
+  buildAPIGateway() {
+
+  }
+
   buildDatabase() {
     this.eventsTable = new Table(this, 'events-table', {
       partitionKey: { name: 'eventId', type: AttributeType.STRING },
       sortKey: { name: 'timestamp', type: AttributeType.STRING }
-
-    })
+    });
   }
+
   buildAggregators() {
+
   }
 }
