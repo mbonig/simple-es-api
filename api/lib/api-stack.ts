c273bc1eb31251576b2151450175f0b1e817e771
Matthew Bonig <matthew.bonig@gmail.com>
2019-07-05 15:01:33 -0600

    updating lambda deploy

--- api/lib/api-stack.ts
+++ api/lib/api-stack.ts
@@ -53,7 +53,7 @@ export class ApiStack extends cdk.Stack {
         },
         handler: 'handlers/index.aggregator',
         runtime: Runtime.NODEJS_10_X,
-        code: Code.bucket(Bucket.fromBucketName(this, 's3_deploy_bucket', this.node.tryGetContext("s3_deploy_bucket")), 'lambda.zip')
+        code: Code.bucket(Bucket.fromBucketName(this, 's3_deploy_bucket', this.node.tryGetContext("s3_deploy_bucket")), 'lambda.zip', 'latest')
       });
       aggregateLambda.addEventSource(new DynamoEventSource(this.eventsTable, { startingPosition: StartingPosition.LATEST }));
       this.aggregateTables.push(aggregateTable);
