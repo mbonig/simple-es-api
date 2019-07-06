6c60705a2c863293e8ab6724deba7675f03e5098
Matthew Bonig <matthew.bonig@gmail.com>
2019-07-05 21:16:38 -0600

    better handler

--- api/lib/api-stack.ts
+++ api/lib/api-stack.ts
@@ -3,6 +3,7 @@ import { Table, AttributeType, BillingMode, StreamViewType } from '@aws-cdk/aws-
 import { Function, Runtime, Code, StartingPosition } from '@aws-cdk/aws-lambda';
 import { DynamoEventSource } from '@aws-cdk/aws-lambda-event-sources';
 import { Bucket } from '@aws-cdk/aws-s3';
+import { PolicyStatement } from '@aws-cdk/aws-iam';
 export interface ApiStackProps extends cdk.StackProps {
   buildAPIGateway: boolean;
   aggregators: string[];
@@ -40,12 +41,14 @@ export class ApiStack extends cdk.Stack {
   buildAggregators(aggregators: string[]) {
     this.aggregateTables = [];
     for (let aggregator of aggregators) {
+
       const aggregateTable = new Table(this, `${aggregator}-view-table`, {
         partitionKey: { name: 'id', type: AttributeType.STRING },
         billingMode: BillingMode.PROVISIONED,
         readCapacity: 3,
         writeCapacity: 3
       });
+
       const aggregateLambda = new Function(this, `${aggregator}-processor`, {
         environment: {
           TABLE_NAME: aggregateTable.tableName,
@@ -56,6 +59,11 @@ export class ApiStack extends cdk.Stack {
         runtime: Runtime.NODEJS_10_X,
         code: Code.bucket(Bucket.fromBucketName(this, 's3_deploy_bucket', this.node.tryGetContext("s3_deploy_bucket")), `${this.node.tryGetContext("lambda_hash")}.zip`)
       });
+      aggregateLambda.addToRolePolicy(new PolicyStatement({
+        actions: ["dynamodb:GetItem", "dynamodb:PutItem"],
+        resources: [aggregateTable.tableArn]
+      }));
+
       aggregateLambda.addEventSource(new DynamoEventSource(this.eventsTable, { startingPosition: StartingPosition.LATEST }));
       this.aggregateTables.push(aggregateTable);
     }
