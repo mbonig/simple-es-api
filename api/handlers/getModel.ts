b01bd4f83f29c1b8c52024faf1882b5cadc7005f
Matthew Bonig <matthew.bonig@gmail.com>
2019-07-20 18:56:04 -0600

    better key usage

--- api/handlers/getModel.ts
+++ api/handlers/getModel.ts
@@ -1,4 +1,5 @@
 import { upperCase } from 'change-case';
+import { ScanInput } from 'aws-sdk/clients/dynamodb';
 const AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient();
 
@@ -16,7 +17,11 @@ async function getModels(aggregate: string, lastEvaluatedKey: string | undefined
     if (!tableName) {
         throw new Error(`Could not find an aggregate table with the name ${aggregate}`);
     }
-    return ddb.scan({ TableName: tableName, Limit: 10 }).promise();
+    const params: any = { TableName: tableName, Limit: 10 };
+    if (lastEvaluatedKey) {
+        params.ExclusiveStartKey = lastEvaluatedKey
+    }
+    return ddb.scan(params).promise();
 
 }
 
