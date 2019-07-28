bd8bfed9dbc75c3de4e1612e473199d28388332b
Matthew Bonig <matthew.bonig@gmail.com>
2019-07-28 17:08:36 -0600

    better way to do aggregates

--- api/bin/api.ts
+++ api/bin/api.ts
@@ -2,13 +2,10 @@
 import 'source-map-support/register';
 import cdk = require('@aws-cdk/core');
 import { ApiStack } from '../lib/api-stack';
-import { readdirSync } from 'fs';
-const path = require('path');
-const app = new cdk.App();
 
-const aggregators = readdirSync(path.resolve('handlers/lib/events'), { withFileTypes: true })
-    .filter(d => d.isDirectory())
-    .map(d => d.name);
+const app = new cdk.App();
+const index = require('../handlers/lib/events');
+const aggregators = Object.keys(index.eventHandlers);
 
 const props = {
     buildAPIGateway: true,
